3790e82332e8912a6f110b982c65c9d98775ddff
김태민 <ktm3628@naver.com>
2022-07-22 09:50:26 +0900

    07-22

--- src/main/java/dao/ProductRepository.java
+++ src/main/java/dao/ProductRepository.java
@@ -15,6 +15,29 @@ public class ProductRepository {
 		phone.setManufacturer("Apple");
 		phone.setUnitsInStock(1000);
 		phone.setCondition("New");
+		
+		Product notebook = new Product("P1235", "LG PC 그램", 1500000);
+		notebook.setDescription("13.3-inch, IPS LED display, 5rd Genneration Intel Core processors");
+		notebook.setCategory("Notebook");
+		notebook.setManufacturer("LG");
+		notebook.setUnitsInStock(1000);
+		notebook.setCondition("Refurbished");
+		
+		Product tablet = new Product("P1236", "Galaxy Tab S", 900000);
+		tablet.setDescription("212.8*125.6*6.6mm, Super AMOLED display, Octa Core processor");
+		tablet.setCategory("Tablet");
+		tablet.setManufacturer("Samsung");
+		tablet.setUnitsInStock(1000);
+		tablet.setCondition("Old");
+		
+		listOfProducts.add(phone);
+		listOfProducts.add(notebook);
+		listOfProducts.add(tablet);
+		
+	}
+	
+	public ArrayList<Product> getAllProducts() {
+		return listOfProducts;
 	}
 
 }
